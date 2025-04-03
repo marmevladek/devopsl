4d3593b8fb129a52e9b63b9f0d6377ad6b7b175e
Vladislav <vladislav.tezev@gmail.com>
2025-04-03 22:39:39 +0300

    Implemented work with the database
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

--- backend-service/src/main/java/ru/devopsl/backendservice/service/impl/ProductServiceImpl.java
+++ backend-service/src/main/java/ru/devopsl/backendservice/service/impl/ProductServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.devopsl.backendservice.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.devopsl.backendservice.mapper.ProductMapper;
 import ru.devopsl.backendservice.model.Product;
+import ru.devopsl.backendservice.payload.request.ProductRequest;
 import ru.devopsl.backendservice.payload.response.MessageResponse;
 import ru.devopsl.backendservice.payload.response.ProductResponse;
 import ru.devopsl.backendservice.repository.ProductRepository;
@@ -15,23 +17,50 @@ public class ProductServiceImpl implements ProductService {
         this.productRepository = productRepository;
     }
 
+
     @Override
-    public MessageResponse addProduct(Product product) {
-        return null;
+    public MessageResponse addProduct(ProductRequest productRequest) {
+        productRepository.save(ProductMapper.mapToProduct(productRequest));
+        return new MessageResponse("Product has been successfully added");
     }
 
     @Override
-    public MessageResponse updateProduct(Product product) {
-        return null;
+    public MessageResponse updateProduct(Long id, ProductRequest productRequest) {
+        Product existingProduct = productRepository.findById(id).orElseThrow(
+                /*
+                () -> new ProductNotFoundException("Product not found")
+                 */
+        );
+
+        existingProduct.setName(productRequest.getName());
+        existingProduct.setDescription(productRequest.getDescription());
+        existingProduct.setPrice(productRequest.getPrice());
+
+        productRepository.save(existingProduct);
+
+        return new MessageResponse("Product has been successfully updated");
     }
 
     @Override
     public MessageResponse deleteProduct(Long id) {
-        return null;
+        Product existingProduct = productRepository.findById(id).orElseThrow(
+                /*
+                () -> new ProductNotFoundException("Product not found")
+                 */
+        );
+
+        productRepository.delete(existingProduct);
+        return new MessageResponse("Product has been successfully deleted");
     }
 
     @Override
     public ProductResponse getProductById(Long id) {
-        return null;
+        Product product = productRepository.findById(id).orElseThrow(
+                /*
+                () -> new ProductNotFoundException("Product not found")
+                 */
+        );
+
+        return ProductMapper.mapToProductResponse(product);
     }
 }

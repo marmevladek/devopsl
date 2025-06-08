597c73868c9fbc998942cc135e25d425e4428920
Vladislav <vladislav.tezev@gmail.com>
2025-06-08 21:27:29 +0300

    Add new DTO for rabbit
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ backend-service/src/main/java/ru/devopsl/backendservice/dto/ProductDTO.java
@@ -0,0 +1,25 @@
+package ru.devopsl.backendservice.dto;
+
+import java.time.LocalDateTime;
+
+public class ProductDTO {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String fullDescription;
+
+    private float price;
+
+    private String linkImage;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private String category;
+}

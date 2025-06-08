46500056b7b8654dec9b660f45de3eed4b11d8ee
Vladislav <vladislav.tezev@gmail.com>
2025-06-08 22:20:33 +0300

    Add lombok annotations
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

--- backend-service/src/main/java/ru/devopsl/backendservice/dto/ProductDTO.java
+++ backend-service/src/main/java/ru/devopsl/backendservice/dto/ProductDTO.java
@@ -1,8 +1,18 @@
 package ru.devopsl.backendservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+
     private Long id;
 
     private String name;

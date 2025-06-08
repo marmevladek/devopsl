2c4c76d278bc5509a8677b547c594b760516b569
Vladislav <vladislav.tezev@gmail.com>
2025-06-08 22:22:32 +0300

    Add notifications
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ notification-service/src/main/java/ru/devopsl/notificationservice/model/Notification.java
@@ -0,0 +1,14 @@
+package ru.devopsl.notificationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    private String text;
+}
